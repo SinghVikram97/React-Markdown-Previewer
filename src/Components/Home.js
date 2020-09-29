3cd0f94a00aefc238322a5b9bab8183cbbdfea24
Vikram <vikram.bedi@paytm.com>
2020-09-29 19:29:52 +0530

    Fetch posts from firebase

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,22 @@ import db from "../firebase";
 import { Link } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 class Home extends Component {
+  state = {
+    posts: [],
+    title: "",
+  };
+  componentDidMount() {
+    // Fetch all posts
+    db.ref("/posts").on("value", (snapshot) => {
+      let posts = [];
+      snapshot.forEach((childSnapshot) => {
+        posts.push({ ...childSnapshot.val(), id: childSnapshot.key });
+      });
+      this.setState({ posts });
+    });
+  }
   render() {
+    console.log(this.state.posts);
     return <>Home</>;
   }
 }
